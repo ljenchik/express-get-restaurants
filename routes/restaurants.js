4b4407f51d2b4888c3c8646fd8327387b17da269
ljenchik <pelagenkolena@gmail.com>
2023-11-09 11:08:47 +0000

    Added server validation for /restaurants endpoint

---
+++ routes/restaurants.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const { Router } = require("express");
+const restaurantsRouter = Router();
+const { Restaurant, Menu, Item } = require("../models/index");
+const { check, validationResult } = require("express-validator");
+restaurantsRouter.use(express.json());
+restaurantsRouter.use(express.urlencoded());
+
+restaurantsRouter.get("/", async (req, res) => {
+    const restaurants = await Restaurant.findAll({
+        include: Menu,
+        include: [
+            {
+                model: Menu,
+                include: [
+                    {
+                        model: Item,
+                    },
+                ],
+            },
+        ],
+    });
+    res.json(restaurants);
+});
+
+restaurantsRouter.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const foundRestaurant = await Restaurant.findByPk(id, {
+        include: Menu,
+        include: [
+            {
+                model: Menu,
+                include: [
+                    {
+                        model: Item,
+                    },
+                ],
+            },
+        ],
+    });
+    if (!foundRestaurant) {
+        res.status(404).end();
+    } else {
+        res.json(foundRestaurant);
+    }
+});
+
+restaurantsRouter.post(
+    "/",
+    [
+        check("name").not().isEmpty().trim(),
+        check("location").not().isEmpty().trim(),
+        check("cuisine").not().isEmpty().trim(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.send({ error: errors.array() });
+        } else {
+            await Restaurant.create({
+                name: req.body.name,
+                location: req.body.location,
+                cuisine: req.body.cuisine,
+            });
+            const allRestaurants = await Restaurant.findAll();
+            res.json(allRestaurants);
+        }
+    }
+);
+module.exports = restaurantsRouter;
--- src/app.js
+++ src/app.js
@@ -1,68 +1,69 @@
 const express = require("express");
 const app = express();
-const { Restaurant, Menu, Item } = require("../models/index");
-app.use(express.json());
-app.use(express.urlencoded());
 
-app.get("/restaurants", async (req, res) => {
-    const restaurants = await Restaurant.findAll({
-        include: Menu,
-        include: [
-            {
-                model: Menu,
-                include: [
-                    {
-                        model: Item,
-                    },
-                ],
-            },
-        ],
-    });
-    res.json(restaurants);
-});
+const restaurantsRouter = require("./../routes/restaurants");
 
-app.get("/restaurants/:id", async (req, res) => {
-    const id = req.params.id;
-    const foundRestaurant = await Restaurant.findByPk(id);
-    if (!foundRestaurant) {
-        res.status(404).end();
-    } else {
-        res.json(foundRestaurant);
-    }
-});
+app.use("/restaurants", restaurantsRouter);
 
-app.post("/restaurants", async (req, res) => {
-    await Restaurant.create({
-        name: req.body.name,
-        location: req.body.location,
-        cuisine: req.body.cuisine,
-    });
-    const allRestaurants = await Restaurant.findAll();
-    res.json(allRestaurants);
-});
+// app.get("/restaurants", async (req, res) => {
+//     const restaurants = await Restaurant.findAll({
+//         include: Menu,
+//         include: [
+//             {
+//                 model: Menu,
+//                 include: [
+//                     {
+//                         model: Item,
+//                     },
+//                 ],
+//             },
+//         ],
+//     });
+//     res.json(restaurants);
+// });
 
-app.put("/restaurants/:id", async (req, res) => {
-    const id = req.params.id;
-    const foundRestaurant = await Restaurant.findByPk(id);
-    if (!foundRestaurant) {
-        res.status(404).end();
-    } else {
-        await foundRestaurant.update(req.body);
-        const allRestaurants = await Restaurant.findAll();
-        res.json(allRestaurants);
-    }
-});
+// app.get("/restaurants/:id", async (req, res) => {
+//     const id = req.params.id;
+//     const foundRestaurant = await Restaurant.findByPk(id);
+//     if (!foundRestaurant) {
+//         res.status(404).end();
+//     } else {
+//         res.json(foundRestaurant);
+//     }
+// });
 
-app.delete("/restaurants/:id", async (req, res) => {
-    const id = req.params.id;
-    const foundRestaurant = await Restaurant.findByPk(id);
-    if (!foundRestaurant) {
-        res.status(404).end();
-    } else {
-        await foundRestaurant.destroy();
-        const allRestaurants = await Restaurant.findAll();
-        res.json(allRestaurants);
-    }
-});
+// app.post("/restaurants", async (req, res) => {
+//     await Restaurant.create({
+//         name: req.body.name,
+//         location: req.body.location,
+//         cuisine: req.body.cuisine,
+//     });
+//     const allRestaurants = await Restaurant.findAll();
+//     res.json(allRestaurants);
+// });
+
+// app.put("/restaurants/:id", async (req, res) => {
+//     const id = req.params.id;
+//     const foundRestaurant = await Restaurant.findByPk(id);
+//     if (!foundRestaurant) {
+//         res.status(404).end();
+//     } else {
+//         await foundRestaurant.update(req.body);
+//         const allRestaurants = await Restaurant.findAll();
+//         res.json(allRestaurants);
+//     }
+// });
+
+// app.delete("/restaurants/:id", async (req, res) => {
+//     const id = req.params.id;
+//     const foundRestaurant = await Restaurant.findByPk(id);
+//     if (!foundRestaurant) {
+//         res.status(404).end();
+//     } else {
+//         await foundRestaurant.destroy();
+//         const allRestaurants = await Restaurant.findAll();
+//         res.json(allRestaurants);
+//     }
+// });
 
 module.exports = app;
